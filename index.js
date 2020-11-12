52b841f746ed2ed3a99c37e3030150cc1309fcfb
Brian Short <brian.ralph.short@gmail.com>
2020-11-11 19:58:03 -0500

    Create internCards array using user input to populate HTML items

--- index.js
+++ index.js
@@ -236,4 +236,21 @@ function buildTeam() {
             </div>`;
         });
         
-        
+        const internCards = internArray.map(intern => {
+            return `
+            <div class="card m-3">
+                <div class="card-header">
+                    <h2 class="card-title">${intern.name}</h2>
+                    <h3 class="card-title"><i class="fas fa-graduation-cap"></i> ${intern.getRole()}</h3>
+                </div>
+                <div class="card-body">
+                    <ul class="list-group">
+                        <li class="list-group-item">ID: ${intern.id}</li>
+                        <li class="list-group-item">Email: <a href="mailto:${intern.email}">${intern.email}</a></li>
+                        <li class="list-group-item">School: ${intern.school}</li>
+                    </ul>
+                </div>
+            </div>
+            `
+    });
+    

c643ce4498b5346aa9d31d6522bb8a7532eb7da6
Brian Short <brian.ralph.short@gmail.com>
2020-11-11 19:56:55 -0500

    Create managerCards array using user input to populate HTML items

--- index.js
+++ index.js
@@ -202,3 +202,21 @@ function buildTeam() {
     
     const buildUnits = (managerArray, engineerArray, internArray) => {
     
+        const managerCards = managerArray.map(manager => {
+            return `
+            <div class="card m-3">
+                <div class="card-header">
+                    <h2 class="card-title">${manager.name}</h2>
+                    <h3 class="card-title"><i class="fas fa-hands-helping"></i> ${manager.role}</h3>
+                </div>
+                <div class="card-body">
+                    <ul class="list-group">
+                        <li class="list-group-item">ID: ${manager.id}</li>
+                        <li class="list-group-item">Email: <a href="mailto:${manager.email}">${manager.email}</a></li>
+                        <li class="list-group-item">Office number: ${manager.officeNumber}</li>    
+                    </ul>
+                </div>
+            </div>`
+        });
+        
+        

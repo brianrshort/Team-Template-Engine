d7ef6c440ccc8f16f710c1e73fa6e94d3ba597ee
Brian Short <brian.ralph.short@gmail.com>
2020-11-11 19:50:51 -0500

    Add employee class

--- index.js
+++ index.js
@@ -3,3 +3,31 @@ const inquirer = require("inquirer");
 const util = require("util")
 
 const writeFileAsync = util.promisify(fs.writeFile);
+
+
+
+class Employee {
+    constructor (name, email, id) {
+        this.name = name;
+        this.email = email;
+        this.id = id; 
+        this.role = "employee";
+        this.getname = function() {
+            return this.name;
+        }
+        this.getId = function() {
+            return this.id;
+        }
+        this.getEmail = function() {
+            return this.email;
+        }
+        this.getRole = function() {
+            return this.role;
+        }
+
+    }
+}
+
+//const phil = new Employee("Phil", 1, "joe@phil.com");
+//console.log(phil);
+
